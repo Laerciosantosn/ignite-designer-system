fa65485b606f12003fbe09023b63ab31ab41d423
laerciosantosn <laerciosantosn@gmail.com>
2022-10-16 13:25:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl text-violet-500">Hello every people</h1>
+
+      <button className="bg-cyan-500 font-medium px-4 py-2 text-white rounded hover:bg-violet-600">
+        Enviar
+      </button>
+    </>
+  )
+}
+
